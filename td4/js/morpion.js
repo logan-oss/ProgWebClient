52173e6db61c358d124cea00c71d91e38b6fbf86
logan-oss <65852078+logan-oss@users.noreply.github.com>
2021-10-14 14:41:14 +0200

    morpion class 14/10/2021

--- td4/js/morpion.js
+++ td4/js/morpion.js
@@ -1,4 +1,5 @@
-import {MorpionSimple} from "./class_morpionSimple.js";
+import {Morpion_Simple} from "./class/class_MorpionSimple";
+import {Morpion} from "./class/class_Morpion";
 
 let morpion;
 let nbCoups;
@@ -12,61 +13,74 @@ let zoneMessage;
 
 const btnReset = document.getElementById('btn_reset');
 btnReset.addEventListener('click', recommence);
+document.getElementsByTagName('BODY')[0].onload = recommence();
 
 function recommence () {
-  const MAX_GRILLE = 8;
-  const MIN_GRILLE = 3;
-
-  // -- affichage -- //
+  let error="";
   zoneMessage = document.getElementById('messages');
   taille = Number.parseInt(document.getElementById('taille').value);
-  modeJeu = document.getElementById('simple').checked ? 'simple' : 'complet';
-  if (Number.isNaN(taille) || taille < MIN_GRILLE || taille > MAX_GRILLE) {
-    zoneMessage.innerHTML = 'Taille invalide !';
-  } else {
+  modeJeu = document.getElementById('simple').checked ? "simple" : "complexe";
+  if(modeJeu === "simple"){
+    try {
+      morpion = new Morpion_Simple(taille);
+    } catch (e) {
+      alert(e);
+      return null;
+    }
+  }
+  else {
+    try {
+      morpion = new Morpion(taille);
+    } catch (e) {
+      alert(e);
+      return null;
+    }
+  }
+
     const table = document.getElementById('table_morpion');
     for (let l = table.rows.length - 1; l >= 0; l--) {
       table.deleteRow(l);
     }
 
-    // -- Init morpion, affichage & object -- //
-    morpion = modeJeu === "simple" ? new MorpionSimple(taille);
-    alert(morpion.getSize());
-    for (let i = 0; i < morpion.getSize(); i++) {
+    for (let i = 0; i < morpion.size; i++) {
       const ligne = table.insertRow(i);
-      morpion[i] = new Array(taille);
-      for (let j = 0; j < taille; j++) {
-        morpion[i][j] = ' ';
-
+      for (let j = 0; j < morpion.size; j++) {
         const id = '' + ((i + 1) * 10 + (j + 1));
         const cell = ligne.insertCell(j);
-        cell.innerHTML = "<input type='button' class='case' id='" + id + "' onclick='clicBouton(this, " + i + ',' + j + ")'/>";
+        cell.innerHTML = "<input type='button' class='case' id='" + id + "'/>";
         document.getElementById(id).value = '';
       }
     }
+
+    let tbtn = document.getElementsByClassName('case');
+    for(let i = 0; i<morpion.size*morpion.size;i++){
+      let id = tbtn[i].getAttribute("id");
+      tbtn[i].addEventListener("click", function(){
+        clicBouton(this,Number.parseInt(id[0])-1,Number.parseInt(id[1])-1);
+      });
+    }
+
 
     nbCoups = 0;
     joueur = 1;
     symbole = 'x';
     zoneMessage.innerHTML = 'Joueur 1, à toi !';
     document.getElementById('btn_reset').disabled = true;
-  }
 }
 
 function clicBouton (uneCase, y, x) {
-  if (morpion[y][x] === ' ') {
-    morpion[y][x] = symbole;
+  if (morpion.clicBouton(symbole, y, x)) {
     uneCase.value = symbole;
     uneCase.classList.add('joueur' + joueur);
     nbCoups++;
 
-    const victoire = aGagne(symbole, y, x);
+    const victoire = morpion.aGagne(symbole, y, x);
     if (victoire) {
       zoneMessage.innerHTML = 'Le joueur ' + joueur + ' a gagné !';
       desactiveEcouteurs();
       symbole === 'x' ? scores[0]++ : scores[1]++;
       document.getElementById('score').innerHTML = 'X : ' + scores[0] + ' - O  : ' + scores[1];
-    } else if (nbCoups === taille * taille) {
+    } else if (nbCoups === morpion.size * morpion.size) {
       zoneMessage.innerHTML = 'Match nul !';
       desactiveEcouteurs();
     } else {
@@ -85,114 +99,13 @@ function clicBouton (uneCase, y, x) {
 }
 
 function desactiveEcouteurs () {
-  for (let i = 0; i < taille; i++) {
-    for (let j = 0; j < taille; j++) {
-      document.getElementById('' + ((i + 1) * 10 + (j + 1))).removeAttribute('onclick');
+  for (let i = 0; i < morpion.size; i++) {
+    for (let j = 0; j < morpion.size; j++) {
+      let btn = document.getElementById('' + ((i + 1) * 10 + (j + 1)));
+      btn.replaceWith(btn.cloneNode(true));                               // Delete event listener
     }
   }
   document.getElementById('btn_reset').disabled = false;
-}
-
-function aGagne (symbole, y, x) {
-  if (modeJeu === 'simple') {
-    return aGagne3ParmiN(symbole, y, x);
-  }
-
-  let nbSymboles;
-
-  // gagné en ligne ?
-  const ligne = y;
-  nbSymboles = 0;
-  for (let col = 0; col < taille; col++) {
-    if (morpion[ligne][col] === symbole) {
-      nbSymboles++;
-    }
-  }
-  if (nbSymboles === taille) {
-    return true;
-  }
-
-  // gagné en colonne ?
-  const col = x;
-  nbSymboles = 0;
-  for (let ligne = 0; ligne < taille; ligne++) {
-    if (morpion[ligne][col] === symbole) {
-      nbSymboles++;
-    }
-  }
-  if (nbSymboles === taille) {
-    return true;
-  }
-
-  // gagné diagonale
-  if (x === y) {
-    nbSymboles = 0;
-    for (let lc = 0; lc < taille; lc++) {
-      if (morpion[lc][lc] === symbole) {
-        nbSymboles++;
-      }
-    }
-    if (nbSymboles === taille) {
-      return true;
-    }
-  }
-
-  // gagné diag inverse
-  if (x === taille - (y + 1)) {
-    nbSymboles = 0;
-    for (let ligne = 0; ligne < taille; ligne++) {
-      if (morpion[ligne][taille - (ligne + 1)] === symbole) {
-        nbSymboles++;
-      }
-    }
-    if (nbSymboles === taille) {
-      return true;
-    }
-  }
-
-  return false;
-}
-
-function aGagne3ParmiN (symbole, y, x) {
-  const aTrouver = symbole.repeat(3);
-
-  // gagné en ligne ? : concaténation de la ligne, et recherche de la sous-chaîne gagnante
-  let ligne = '';
-  morpion[y].forEach(element => (ligne += element));
-  if (ligne.indexOf(aTrouver) >= 0) {
-    return true;
-  }
-
-  // gagné en colonne ? : concaténation de la colonne et recherche de la sous-chaîne gagnante
-  let col = '';
-  morpion.forEach(element => (col += element[x]));
-  if (col.indexOf(aTrouver) >= 0) {
-    return true;
-  }
-
-  // gagné diagonale
-  if (x === y) {
-    let diagonale = '';
-    for (let lc = 0; lc < taille; lc++) {
-      diagonale += morpion[lc][lc];
-    }
-    if (diagonale.indexOf(aTrouver) >= 0) {
-      return true;
-    }
-  }
-
-  // gagné diag inverse
-  if (x === taille - (y + 1)) {
-    let inverse = '';
-    for (let lc = 0; lc < taille; lc++) {
-      inverse += morpion[lc][taille - (lc + 1)];
-    }
-    if (inverse.indexOf(aTrouver) >= 0) {
-      return true;
-    }
-  }
-
-  return false;
 }
 
 document.getElementById('score').innerHTML = 'X : ' + scores[0] + ' - O  : ' + scores[1];
